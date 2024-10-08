f82fe51887c9af9e2d450d7310456a6c769a9c84
Z0d1akAllSeeingEye <andriirudyk32@gmail.com>
2024-10-08 08:58:45 +0200

    "Specified who adds 'Adder' and who adds ' Substractor'"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 // TODO: we need to add the missing classes!
-//Ok, I will add 'Adder' and s3930 will add 'Substractor'
+//Ok, I will add 'Adder' and s3930 will add 'Substractor'.
 
 public class Main {
     public static void main(String[] args ){
